977d2e940e729803dd349aff3cafd7db27ed8a71
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2022-09-15 12:17:07 +0600

    function

---
+++ function.js
@@ -0,0 +1,11 @@
+
+function sayLoveYou(){
+    console.log('i am a student');
+    console.log('i am 20');
+}
+sayLoveYou();
+var date =23;
+var place="restaurent";
+sayLoveYou();
+var hangout ="tsc";
+sayLoveYou();
