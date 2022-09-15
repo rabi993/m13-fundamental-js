f6afa7f87eb1cf387eba137dd3ac48248fd2a6e1
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2022-09-15 12:32:06 +0600

    function double parameter/multiple parameter

--- function.js
+++ function.js
@@ -1,11 +1,41 @@
 
-function sayLoveYou(){
-    console.log('i am a student');
-    console.log('i am 20');
+// function sayLoveYou(){
+//     console.log('i am a student');
+//     console.log('i am 20');
+// }
+// sayLoveYou();
+// var date =23;
+// var place="restaurent";
+// sayLoveYou();
+// var hangout ="tsc";
+// sayLoveYou();
+
+// function doubleIt(num){
+//     var result =num*2;
+//     console.log(result);
+
+// }
+// doubleIt(5);
+// doubleIt(25);
+// doubleIt(544);
+
+function doubleIt(num){
+    var result =num*2;
+    return result;
+
 }
-sayLoveYou();
-var date =23;
-var place="restaurent";
-sayLoveYou();
-var hangout ="tsc";
-sayLoveYou();
+// var first = doubleIt(5);
+// var second = doubleIt(26);
+// console.log(first, second);
+var first = doubleIt(5);
+var second = doubleIt(26);
+total =first+ second;
+console.log(total);
+// gkrughdrkguhdrkugh
+console.log('function double parameter/multiple parameter');
+function add(num1,num2){
+    var result = num1+ num2;
+    return result;
+}
+var sum = add(15,23);
+console.log(sum);
