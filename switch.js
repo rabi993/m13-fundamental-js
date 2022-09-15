8e609d45d152abf4d8c51b1d41c1fd7dc9437f7f
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2022-09-15 12:06:26 +0600

    switch

---
+++ switch.js
@@ -0,0 +1,23 @@
+
+// num=5;
+// if(num>1000){}
+// else if(num>1000){}
+// else if(num>1000){}
+// else if(num>1000){}
+// else if(num>1000){}
+num = 5
+switch(num){
+    case 1000:
+        console.log('i am 1000');
+         break;
+    case 100:
+        console.log('i am 100');
+         break;
+    case 20:
+    case 10:
+        console.log('i am 10');
+         break;
+    default:
+        console.log('i dont know who you are');
+        
+}
