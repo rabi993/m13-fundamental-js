9e378e364362b7432bbe1dbc0a05dbc0b756614e
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2022-09-15 12:51:54 +0600

    properties of student

---
+++ object.js
@@ -0,0 +1,15 @@
+
+var student1 = {id:121,phone:54134,name:"rabi"};
+var student2 = {id:122,phone:5414474,name:"kamal"};
+
+
+console.log(student1);
+console.log(student2);
+var phonNo =student1.phone;
+console.log(phonNo);
+var phonN2 =student2['phone'];
+console.log(phonN2);
+var phonPropName ="phone";
+var phonN3 =student2[phonPropName];
+console.log(phonN3);
+
